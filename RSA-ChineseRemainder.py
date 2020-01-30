58f634a908f4f569be11579c4127b90affb29ef2
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2020-01-30 13:32:56 +0530

    Add files via upload

---
+++ RSA-ChineseRemainder.py
@@ -0,0 +1,15 @@
+def encrypt(x, n):
+	return (x**3 % n)
+def solvemod(N, n):
+	for i in range(n):
+		if (N*i % n == 1):
+			return i
+	return 0
+def decrypt(x,p,q):
+	xp = x%p
+	xq = x%q
+	np = q
+	nq = p
+	ap = solvemod(np, p)
+	aq = solvemod(nq, q)
+	return xq*ap*nq + xp*aq*np
