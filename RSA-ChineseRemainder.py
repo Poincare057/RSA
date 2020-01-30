78579cacd80bab1707e5131cc069d6433bfe9386
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2020-01-30 19:39:00 +0530

    Update RSA-ChineseRemainder.py

--- RSA-ChineseRemainder.py
+++ RSA-ChineseRemainder.py
@@ -5,9 +5,16 @@ def solvemod(N, n):
 		if (N*i % n == 1):
 			return i
 	return 0
+
+#Chinese Remainder
 def decrypt(x, p, q):
 	xp = x%p
 	xq = x%q
 	ap = solvemod(q,p)
 	aq = solvemod(p,q)
 	return (xp*q*ap + xq*p*aq)
+
+#Euler's theorem
+def decrypt(x, p, q, e):
+	d = solvemod(e, (p-1)*(q-1))
+	return (x**d)%(p*q)
