271ca391bb0fd095d3c8b40cc38dab04f6e8da42
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2020-02-01 14:40:54 +0530

    Update RSA_encrypt_decrypt.py

--- RSA_encrypt_decrypt.py
+++ RSA_encrypt_decrypt.py
@@ -1,3 +1,4 @@
+#O(log(a)^(log(3))*log(b)) modular exponentiation for a^b mod n
 def fastmodexp(a, b, n):
 	import numpy as np
 	k = len(bin(b)) - 2
@@ -12,16 +13,19 @@ def fastmodexp(a, b, n):
 		if int(s[i]) == 1:
 			p = (p*l[i])%n
 	return int(p)
-  
+
+#Solution of linear congruence Nx mod n = 1
 def solvemod(N, n):
     for i in range(n):
         if (N*i % n == 1):
           return i
     return 0
 
+#RSA Encryption
 def encrypt(x, n, e):
     return fastmodexp(x, e, n)
 
+#RSA Decryption
 def decrypt(x, p, q, e):
 	d = solvemod(e, (p-1)*(q-1))
 	return fastmodexp(x, d, p*q)
