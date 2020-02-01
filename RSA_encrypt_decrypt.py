b08208100d6ece7b819ad7b5f66f60722e992a30
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2020-02-01 16:05:01 +0530

    Update RSA_encrypt_decrypt.py

--- RSA_encrypt_decrypt.py
+++ RSA_encrypt_decrypt.py
@@ -14,18 +14,28 @@ def fastmodexp(a, b, n):
 			p = (p*l[i])%n
 	return int(p)
 
-#Solution of linear congruence Nx mod n = 1
-def solvemod(N, n):
-    for i in range(n):
-        if (N*i % n == 1):
-          return i
-    return 0
+#Extended Euclidean Algorithm that solves bx mod a = 1; returns (x,y) pair where ax + by = 1
+def exteuc(a, b):
+	A = [a, b]
+	Q = [a//b]
+	X = [0,0,1]
+	Y=[0,1,-Q[0]]
+	c = a%b
+	A += [c]
+	while c > 1:
+		a, b = b, c
+		Q += [a//b]
+		c = a%b
+		X += [X[len(X) - 2] - X[len(X)-1]*Q[len(X)-2]]
+		Y += [Y[len(Y) - 2] - Y[len(Y)-1]*Q[len(Y)-2]]
+	return [X[len(X)-1], Y[len(Y)-1]]
 
 #RSA Encryption
 def encrypt(x, n, e):
     return fastmodexp(x, e, n)
 
-#RSA Decryption
+#RSA Decryption 
 def decrypt(x, p, q, e):
-	d = solvemod(e, (p-1)*(q-1))
+	d = (exteuc((p-1)*(q-1), e)[1])%((p-1)*(q-1)) #a = (p-1)*(q-1), b = e. Have to take y from extended euclid mod (p-1)*(q-1) to get d. 
 	return fastmodexp(x, d, p*q)
+
